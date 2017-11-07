7a8ac593409cac12c6c7d7f3642808a89cd98a69
Sergey Stotskiy <sergey.stotskiy@namecheap.com>
2017-11-07 16:36:08 +0200

    chore(build): adds a separate build for hybrid version of ES5 + ES6 modules
    Mainly needed for dev envs like webpack

---
+++ tools/rollup.es5m.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+import config from './rollup.es';
+
+export default Object.assign({}, config, {
+  dest: 'index.js',
+  format: 'es',
+  plugins: config.plugins.concat([
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        ['es2015', { modules: false }]
+      ],
+      plugins: ['external-helpers']
+    })
+  ])
+});
