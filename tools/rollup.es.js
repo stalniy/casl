738407a2688766ee4432e5be23bb98b47dd0acb3
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-02-22 21:41:32 +0200

    refactor(package): strips out sift from resulting build
    Now it's build as external dependency
    
    Fixes #21

--- tools/rollup.es.js
+++ tools/rollup.es.js
@@ -1,18 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/es6/index.js',
     format: 'es'
   },
-  plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
-    commonjs()
-  ]
+  plugins: []
 };
