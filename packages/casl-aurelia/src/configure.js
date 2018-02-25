0a9d766b94cadea8d9bc23db1d3bd66cd8450434
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-02-25 21:51:49 +0200

    chore(eslint): fixes eslint
    Relates to #26

--- packages/casl-aurelia/src/configure.js
+++ packages/casl-aurelia/src/configure.js
@@ -3,13 +3,11 @@ import { signalBindings } from 'aurelia-binding';
 import { ABILITY_CHANGED_SIGNAL } from './value-converter/can';
 
 export default function configureAbility(config, providedAbility) {
-  let ability;
-
   if (providedAbility && providedAbility instanceof Ability) {
     config.container.registerInstance(Ability, providedAbility);
   }
 
-  ability = config.container.get(Ability);
+  const ability = config.container.get(Ability);
 
   return ability.on('updated', () => signalBindings(ABILITY_CHANGED_SIGNAL));
 }
