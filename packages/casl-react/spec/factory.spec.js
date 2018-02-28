9573650d640506319e2916b1ddd2f380b603d1c0
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-02-28 11:43:37 +0200

    test(react): adds tests into casl react integration
    Fixes #23

---
+++ packages/casl-react/spec/factory.spec.js
@@ -0,0 +1,69 @@
+import { createElement as e, createContext } from 'react'
+import renderer from 'react-test-renderer'
+import { AbilityBuilder } from '@casl/ability'
+import { Can, createCanBoundTo, createContextualCan } from '../src'
+
+describe('Factory methods which create `Can` component', () => {
+  let ability
+  let child
+
+  beforeEach(() => {
+    ability = AbilityBuilder.define(can => can('read', 'Post'))
+    child = spy(returns => e('p', null, 'children'))
+  })
+
+  describe('`createCanBoundTo`', () => {
+    let BoundCan
+
+    beforeEach(() => {
+      BoundCan = createCanBoundTo(ability)
+    })
+
+    it('creates another component with bound ability instance', () => {
+      const component = renderer.create(e(BoundCan, { run: 'read', on: 'Post' }, child))
+
+      expect(component.toJSON().children).to.deep.equal([child().props.children])
+    })
+
+    it('extends `Can` component', () => {
+      const component = renderer.create(e(BoundCan, { run: 'read', on: 'Post' }, child))
+      const instance = component.getInstance()
+
+      expect(instance).to.be.instanceof(Can)
+      expect(instance).to.be.instanceof(BoundCan)
+    })
+
+    it('allows to override ability by passing "ability" property', () => {
+      const anotherAbility = AbilityBuilder.define(can => can('update', 'Post'))
+      const component = renderer.create(e(BoundCan, { run: 'read', on: 'Post', ability: anotherAbility }, child))
+
+      expect(component.toJSON()).to.be.empty
+    })
+  })
+
+  describe('`createContextualCan`', () => {
+    let AbilityContext
+    let ContextualCan
+
+    beforeEach(() => {
+      AbilityContext = createContext()
+      ContextualCan = createContextualCan(AbilityContext.Consumer)
+    })
+
+    it('allows to override `Ability` instance by passing it in props', () => {
+      const element = e(ContextualCan, { run: 'read', on: 'Post', ability }, child)
+      const component = renderer.create(element)
+
+      expect(child).to.have.been.called.with.exactly(ability)
+    })
+
+    it('expects `Ability` instance to be provided by context Provider', () => {
+      const App = e(AbilityContext.Provider, { value: ability },
+        e(ContextualCan, { run: 'read', on: 'Post' }, child)
+      )
+      const component = renderer.create(App)
+
+      expect(child).to.have.been.called.with.exactly(ability)
+    })
+  })
+})
--- packages/casl-react/src/factory.js
+++ packages/casl-react/src/factory.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import { createElement } from 'react';
+import PropTypes from 'prop-types';
+import { Ability } from '@casl/ability';
 import Can from './Can';
 
 export function createCanBoundTo(ability) {
   return class BoundCan extends Can {
-    __ability = ability;
-  }
+    static propTypes = Object.assign({}, Can.propTypes, {
+      ability: PropTypes.instanceOf(Ability)
+    });
+
+    constructor(...args) {
+      super(...args);
+      this.state.ability = this.state.ability || ability;
+    }
+  };
 }
 
-export createContextualCan(Consumer) {
-  return function ContextualCan({ props }) {
-    return React.createElement(Consumer, null, (ability) => {
-      return React.createElement(Can, {
-        ability,
-        run: props.run,
-        on: props.on,
-        children: props.children
-      });
-    });
-  }
+export function createContextualCan(Consumer) {
+  return function ContextualCan(props) {
+    return createElement(Consumer, null, ability => createElement(Can, {
+      ability: props.ability || ability,
+      run: props.run,
+      on: props.on,
+      children: props.children
+    }));
+  };
 }
