db6bbed60941cc3160fb74db22cb2772ac8990da
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-03-13 08:39:00 +0200

    refactor(ability): renames @casl/ability/query to @casl/ability/extra

--- packages/casl-mongoose/src/mongo.js
+++ packages/casl-mongoose/src/mongo.js
@@ -1,4 +1,4 @@
-import { rulesToQuery } from '@casl/ability/query';
+import { rulesToQuery } from '@casl/ability/extra';
 
 function ruleToMongoQuery(rule) {
   return rule.inverted ? { $nor: [rule.conditions] } : rule.conditions;
