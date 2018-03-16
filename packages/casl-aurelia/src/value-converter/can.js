df4ee881c86a3a270abd9349616d0cb7d14356f1
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-03-16 23:16:57 +0200

    chore(aurelia): fixes eslint

--- packages/casl-aurelia/src/value-converter/can.js
+++ packages/casl-aurelia/src/value-converter/can.js
@@ -2,7 +2,9 @@ import { signalBindings } from 'aurelia-binding';
 import { Ability } from '@casl/ability';
 
 const ABILITY_CHANGED_SIGNAL = 'caslAbilityChanged';
-const ABILITY_HAS_SUBSCRIPTION_FIELD = typeof Symbol === 'undefined' ? `__hasAuSubscription${Date.now()}` : Symbol('hasAuSubscription')
+const ABILITY_HAS_SUBSCRIPTION_FIELD = typeof Symbol === 'undefined'
+  ? `__hasAuSubscription${Date.now()}`
+  : Symbol('hasAuSubscription');
 
 export class CanValueConverter {
   signals = [ABILITY_CHANGED_SIGNAL];
@@ -16,7 +18,7 @@ export class CanValueConverter {
   toView(subject, action) {
     if (!this.ability[ABILITY_HAS_SUBSCRIPTION_FIELD]) {
       this.ability.on('updated', () => signalBindings(ABILITY_CHANGED_SIGNAL));
-      this.ability[ABILITY_HAS_SUBSCRIPTION_FIELD] = true
+      this.ability[ABILITY_HAS_SUBSCRIPTION_FIELD] = true;
     }
 
     return this.ability.can(action, subject);
