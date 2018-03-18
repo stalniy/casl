b382e91dc98544b0a212fe59b5dcfe8951f8bbef
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-03-18 15:13:37 +0200

    refactor(aurelia): correctly exposes CanValueConverter

--- packages/casl-aurelia/src/index.js
+++ packages/casl-aurelia/src/index.js
@@ -1,9 +1,16 @@
 import { Ability } from '@casl/ability';
+import { ValueConverterResource } from 'aurelia-framework';
+import { CanValueConverter } from './value-converter/can';
 
 export { CanValueConverter } from './value-converter/can';
-
 export function configure(config, providedAbility) {
-  config.globalResources('./value-converter/can');
+  // TODO: replace with easier API after https://github.com/aurelia/framework/pull/858 is done
+  config.postTask(() => {
+    const resource = new ValueConverterResource('can');
+
+    resource.initialize(config.container, CanValueConverter);
+    resource.register(config.aurelia.resources);
+  });
 
   if (providedAbility && providedAbility instanceof Ability) {
     config.container.registerInstance(Ability, providedAbility);
