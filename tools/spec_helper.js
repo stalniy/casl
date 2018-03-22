1170b690fb1774153f44df341d6220560fc7eb27
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-03-22 12:17:38 +0200

    refactor(tools): removes redundant code

--- tools/spec_helper.js
+++ tools/spec_helper.js
@@ -9,25 +9,4 @@
 })(function(chai, global) {
   global.expect = chai.expect;
   global.spy = chai.spy;
-  chai.Assertion.addMethod('allow', function(action, subject) {
-    const subjectRepresantation = prettifyObject(subject)
-    this.assert(
-      this._obj.can(action, subject),
-      `expected ability to allow ${action} on ${subjectRepresantation}`,
-      `expected ability to not allow ${action} on ${subjectRepresantation}`
-    );
-  });
-
-  function prettifyObject(object) {
-    if (!object || typeof object === 'string') {
-      return object;
-    }
-
-    if (typeof object === 'function') {
-      return object.name;
-    }
-
-    const attrs = JSON.stringify(object);
-    return `${object.constructor.name} { ${attrs[0] === '{' ? attrs.slice(1, -1) : attrs} }`
-  }
 });
