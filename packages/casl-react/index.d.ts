37718fe3c90d232972bd2f0fa070c8773c8bed62
Francois Skorzec <francois@skorzec.fr>
2018-04-20 17:48:54 +0200

    feat(react): adds typescript definition for package @casl/react
    Relates to #54

---
+++ packages/casl-react/index.d.ts
@@ -0,0 +1,23 @@
+import { PureComponent, createElement, Consumer } from 'react';
+import { Ability } from '@casl/ability';
+
+/**
+ * Base properties shared between Can and ContextualCan
+ */
+type BaseProps = {
+  do : string          ;
+  on : Object | string ;
+};
+
+
+type CanProps = BaseProps & {
+  ability : Ability;  
+}
+
+type ContextualCanDelegate = (props: BaseProps) => React.PureComponent<BaseProps>
+
+// Class definition
+export default class Can extends PureComponent<CanProps>{}
+
+export function createCanBoundTo(ability: Ability): ContextualCanDelegate;
+export function createContextualCan<T>(Consumer: Consumer<T>): ContextualCanDelegate;
