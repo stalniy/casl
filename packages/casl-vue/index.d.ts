a7e22518df45e21f88f3baf4ffa23107076f7957
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-05-29 13:08:42 +0300

    fix(vue): fixes issue with wrong type definition for `abilitiesPlugin`

--- packages/casl-vue/index.d.ts
+++ packages/casl-vue/index.d.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { Ability } from '@casl/ability'
 
-export function abilitiesPlugin(Vue: Vue, providedAbility?: Ability): void
+export function abilitiesPlugin(Vue: typeof Vue, providedAbility?: Ability): void
 
 declare module "vue/types/vue" {
   interface Vue {
