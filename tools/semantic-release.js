23a5d07873a71fb921586cb1bebd4cc74c4a802d
Sergii <sergiy.stotskiy@gmail.com>
2018-05-30 16:27:57 +0300

    chore(tools): updates semantic release config to commit changelog & package version on release

--- tools/semantic-release.js
+++ tools/semantic-release.js
@@ -2,15 +2,15 @@ const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME || process.env.npm_package_n
 
 module.exports = {
   verifyConditions: [
-    '@semantic-release/git',
-    '@semantic-release/github',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/changelog'
+    '@semantic-release/git',
+    '@semantic-release/github'
   ],
   prepare: [
-    { path: '@semantic-release/git', message: `chore(release): ${PACKAGE_NAME}@\${nextRelease.version} [skip ci]` },
     '@semantic-release/changelog',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
+    { path: '@semantic-release/git', message: `chore(release): ${PACKAGE_NAME}@\${nextRelease.version} [skip ci]` }
   ],
   tagFormat: `${PACKAGE_NAME}@\${version}`
 }
