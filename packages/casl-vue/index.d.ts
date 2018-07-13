f9e2e7e0712c7b30271c3ca6b5b7df3d852a333c
Andrew Scott <ascott18@gmail.com>
2018-07-13 11:26:31 -0700

    Fix #92

--- packages/casl-vue/index.d.ts
+++ packages/casl-vue/index.d.ts
@@ -1,7 +1,7 @@
-import Vue from 'vue'
+import Vue, { VueConstructor } from 'vue'
 import { Ability } from '@casl/ability'
 
-export function abilitiesPlugin(Vue: typeof Vue, providedAbility?: Ability): void
+export function abilitiesPlugin(Vue: VueConstructor, providedAbility?: Ability): void
 
 declare module "vue/types/vue" {
   interface Vue {
