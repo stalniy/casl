b9732821319e0919877734bf829cbe8906e1bf24
Sergii <sergiy.stotskiy@gmail.com>
2019-12-22 16:35:35 +0200

    feat(ability): adds possibility to specify custom error messages
    Fixes #241

--- packages/casl-ability/src/error.js
+++ packages/casl-ability/src/error.js
@@ -1,18 +1,65 @@
-export function ForbiddenError(message, options = {}) {
-  Error.call(this);
-  this.constructor = ForbiddenError;
-  this.subject = options.subject;
-  this.subjectName = options.subjectName;
-  this.action = options.action;
-  this.field = options.field;
-  this.message = message || `Cannot execute "${this.action}" on "${this.subjectName}"`;
+const getDefaultMessage = error => `Cannot execute "${error.action}" on "${error.subjectName}"`;
+let defaultErrorMessage = getDefaultMessage;
 
-  if (typeof Error.captureStackTrace === 'function') {
-    this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
-  } else {
-    this.stack = new Error(this.message).stack;
+export class ForbiddenError extends Error {
+  static setDefaultMessage(messageOrFn) {
+    if (messageOrFn === null) {
+      defaultErrorMessage = getDefaultMessage;
+    } else {
+      defaultErrorMessage = typeof messageOrFn === 'string' ? () => messageOrFn : messageOrFn;
+    }
+  }
+
+  static from(ability) {
+    const error = new this('');
+    Object.defineProperty(error, 'ability', { value: ability });
+    return error;
+  }
+
+  constructor(message, options = {}) {
+    super(message);
+    this._setMetadata(options);
+    this.message = message || defaultErrorMessage(this);
+    this._customMessage = null;
+
+    if (typeof Error.captureStackTrace === 'function') {
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  setMessage(message) {
+    this._customMessage = message;
+    return this;
+  }
+
+  throwUnlessCan(action, subject, field) {
+    if (!this.ability) {
+      throw new ReferenceError('Cannot throw FordiddenError without respective ability instance');
+    }
+
+    const rule = this.ability.relevantRuleFor(action, subject, field);
+
+    if (rule && !rule.inverted) {
+      return;
+    }
+
+    this._setMetadata({
+      action,
+      subject,
+      field,
+      subjectName: this.ability.subjectName(subject)
+    });
+
+    const reason = rule ? rule.reason : '';
+    this.message = this._customMessage || reason || defaultErrorMessage(this);
+    throw this; // eslint-disable-line
+  }
+
+  _setMetadata(options) {
+    this.subject = options.subject;
+    this.subjectName = options.subjectName;
+    this.action = options.action;
+    this.field = options.field;
   }
 }
-
-ForbiddenError.prototype = Object.create(Error.prototype);
