dfbf59ae44f7364d6f474306320c2a5ca2546bad
Sergii <sergiy.stotskiy@gmail.com>
2020-02-17 14:41:28 +0200

    chore(release): removes semantic-release-monorepo and adds simpler hack for monorepos

--- tools/semantic-release.js
+++ tools/semantic-release.js
@@ -1,3 +1,18 @@
+const parser = require('git-log-parser'); // eslint-disable-line
+
+// this is hack which allows to use semantic-release for monorepo
+// https://github.com/semantic-release/semantic-release/issues/193#issuecomment-578436666
+parser.parse = (parse => (config, options) => {
+  if (Array.isArray(config._)) {
+    config._.push(options.cwd);
+  } else if (config._) {
+    config._ = [config._, options.cwd];
+  } else {
+    config._ = options.cwd;
+  }
+  return parse(config, options);
+})(parser.parse);
+
 const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME || process.env.npm_package_name;
 
 module.exports = {
