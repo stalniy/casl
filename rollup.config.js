631afa055755ddf4fbf8f7830076229ead2f14a2
Sergii <sergiy.stotskiy@gmail.com>
2020-08-10 14:36:02 +0300

    refactor(build): simplifies rollup, adds es6cjs type, renames es6 folder to es6m
    Fixes build for @casl/ability

--- rollup.config.js
+++ rollup.config.js
@@ -2,100 +2,75 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-function aggregate(configs, optionsOverrides) {
-  let external = [];
-  let globals = {};
+const build = config => ({
+  input: config.input || 'src/index.ts',
+  output: {
+    sourcemap: true,
+    format: config.format,
+    dir: `dist/${config.id}`,
+    name: config.name,
+    plugins: [
+      config.minify
+        ? terser({
+          mangle: { properties: { regex: /^_[a-z]/i } }
+        })
+        : null
+    ]
+  },
+  plugins: [
+    resolve({
+      mainFields: ['es2015', 'module', 'main', 'browser'],
+      extensions: ['.js', '.mjs', '.ts'],
+    }),
+    babel({
+      rootMode: 'upward',
+      extensions: ['.js', '.mjs', '.ts'],
+      inputSourceMap: config.useInputSourceMaps,
+      babelHelpers: 'bundled',
+      caller: {
+        output: config.type,
+      }
+    }),
+    ...(config.plugins || [])
+  ]
+});
 
-  if (optionsOverrides.external) {
-    external = optionsOverrides.external.split(',');
+function parseOptions(overrideOptions) {
+  const options = {
+    external: [],
+    useInputSourceMaps: !!process.env.USE_SRC_MAPS,
+    minify: process.env.NODE_ENV === 'production' && process.env.LIB_MINIFY !== 'false'
+  };
+
+  if (overrideOptions.external) {
+    options.external = overrideOptions.external.split(',');
   }
 
-  if (optionsOverrides.globals) {
-    globals = optionsOverrides.globals.split(',').reduce((map, chunk) => {
+  if (overrideOptions.globals) {
+    options.globals = overrideOptions.globals.split(',').reduce((map, chunk) => {
       const [moduleId, globalName] = chunk.split(':');
       map[moduleId] = globalName;
       return map;
     }, {});
-    external = external.concat(Object.keys(globals));
+    options.external.push(...Object.keys(options.globals));
   }
 
-  const buildVariations = process.env.BUILD_TYPES
-    ? process.env.BUILD_TYPES.split(',')
-    : configs.map(config => config.id);
-  const buildTypes = buildVariations.reduce((types, chunk) => {
-    const [type, format] = chunk.split(':');
-    types[type] = { format };
-    return types;
-  }, {});
-
-  return configs
-    .filter(config => buildTypes[config.id])
-    .map(config => ({
-      external,
-      input: optionsOverrides.input || config.input || 'src/index.ts',
-      output: {
-        sourcemap: true,
-        ...config.output,
-        globals,
-        format: buildTypes[config.id].format || config.output.format,
-        plugins: process.env.NODE_ENV === 'production' && process.env.LIB_MINIFY !== 'false'
-          ? [terser({
-            mangle: {
-              properties: {
-                reserved: [
-                  '_collection',
-                  '__esModule',
-                  '__forbiddenByCasl__'
-                ],
-                regex: /^_[a-z]/i
-              }
-            }
-          })]
-          : []
-      },
-      plugins: [
-        resolve({
-          mainFields: ['module', 'main', 'browser'],
-          extensions: ['.js', '.mjs', '.ts'],
-        }),
-        babel({
-          rootMode: 'upward',
-          extensions: ['.js', '.mjs', '.ts'],
-          inputSourceMap: !!process.env.USE_SRC_MAPS,
-          babelHelpers: 'bundled',
-          caller: {
-            output: config.type,
-          }
-        }),
-        ...(config.plugins || [])
-      ]
-    }));
+  return options;
 }
 
-export default overrideOptions => aggregate([
-  {
-    id: 'es6',
-    type: 'es6',
-    output: {
-      dir: 'dist/es6',
-      format: 'es'
-    },
-  },
-  {
-    id: 'es5m',
-    type: 'es5',
-    output: {
-      dir: 'dist/es5m',
-      format: 'es'
-    }
-  },
-  {
-    id: 'umd',
-    type: 'es5',
-    output: {
-      dir: 'dist/umd',
-      format: 'umd',
-      name: overrideOptions.name,
-    },
-  },
-], overrideOptions);
+export default (overrideOptions) => {
+  const builds = [
+    { id: 'es6m', type: 'es6', format: 'es' },
+    { id: 'es6c', type: 'es6', format: 'cjs' },
+    { id: 'es5m', type: 'es5', format: 'es' },
+    { id: 'umd', type: 'es5', format: 'umd', name: overrideOptions.name },
+  ];
+  const config = parseOptions(overrideOptions);
+  const buildTypes = process.env.BUILD_TYPES
+    ? process.env.BUILD_TYPES.split(',')
+    : builds.map(buildConfig => buildConfig.id);
+
+  return builds
+    .filter(buildType => buildTypes.includes(buildType.id))
+    .map(buildType => build({ ...buildType, ...config }));
+};
