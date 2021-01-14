60d0f89ccf87c35c985777abc4f14c3184cfa4a4
Sergii <sergiy.stotskiy@gmail.com>
2021-01-14 13:29:28 +0200

    chore(deps): updates semantic-release libs and config

--- tools/semantic-release.js
+++ tools/semantic-release.js
@@ -15,23 +15,25 @@ parser.parse = (parse => (config, options) => {
 
 module.exports = {
   tagFormat: `${process.env.npm_package_name}@\${version}`,
-  branches: ['master', { name: 'next', channel: 'next', prerelease: true }],
-  verifyConditions: [
-    '@semantic-release/changelog',
+  branches: [
+    'master',
+    { name: 'next', channel: 'next', prerelease: true }
+  ],
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'chore', scope: 'deps', release: 'patch' },
+        { type: 'docs', scope: 'README', release: 'patch' },
+      ]
+    }],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogTitle: '# Change Log\n\nAll notable changes to this project will be documented in this file.'
+    }],
     '@semantic-release/npm',
-    '@semantic-release/git',
+    ['@semantic-release/git', {
+      message: `chore(release): ${process.env.npm_package_name}@\${nextRelease.version} [skip ci]`
+    }],
     '@semantic-release/github'
   ],
-  prepare: [
-    {
-      path: '@semantic-release/changelog',
-      changelogTitle: '# Change Log\n\nAll notable changes to this project will be documented in this file.'
-    },
-    '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      message: `chore(release): ${process.env.npm_package_name}@\${nextRelease.version} [skip ci]`
-    }
-  ],
-  success: []
 };
