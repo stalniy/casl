e5edc92a1964626416933a7315faa4031a72fbc6
Sergii <sergiy.stotskiy@gmail.com>
2021-08-11 16:56:52 +0300

    chore: semantic-release

--- tools/semantic-release.js
+++ tools/semantic-release.js
@@ -18,7 +18,7 @@ module.exports = {
   branches: [
     'master',
     { name: 'next', channel: 'next', prerelease: true },
-    `${process.env.npm_package_name}@+([0-9])?(.{+([0-9]),x}).x`,
+    `+([0-9])?(.{+([0-9]),x}).x`,
   ],
   verifyConditions: [
     '@semantic-release/changelog',
