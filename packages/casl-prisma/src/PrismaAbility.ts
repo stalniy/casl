03c92004e295bc782db21df30d7140fdf955ae8f
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2022-04-30 00:54:09 +0200

    fix: add 'all' to AbilityTuple
    Fixes issue https://github.com/stalniy/casl/issues/584

--- packages/casl-prisma/src/PrismaAbility.ts
+++ packages/casl-prisma/src/PrismaAbility.ts
@@ -3,7 +3,7 @@ import { AbilityOptions, AbilityTuple, fieldPatternMatcher, PureAbility, RawRule
 import { PrismaQuery, prismaQuery } from './prisma/PrismaQuery';
 
 export class PrismaAbility<
-  A extends AbilityTuple = [string, Prisma.ModelName],
+  A extends AbilityTuple = [string, 'all' | Prisma.ModelName],
   C extends PrismaQuery = PrismaQuery
 > extends PureAbility<A, C> {
   constructor(rules?: RawRuleFrom<A, C>[], options?: AbilityOptions<A, C>) {
