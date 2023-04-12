9da8f7c462f62e7fbe93bc7b92a21d56ab24e763
Aleksey Druzhinin <2452452@gmail.com>
2023-04-12 12:34:54 +0400

    chore: add origin prop at rule class

--- packages/casl-ability/src/Rule.ts
+++ packages/casl-ability/src/Rule.ts
@@ -42,7 +42,7 @@ export class Rule<A extends Abilities, C> {
   public readonly conditions!: C | undefined;
   public readonly fields!: string[] | undefined;
   public readonly reason!: string | undefined;
-  public readonly additionalData!: any;
+  public readonly origin!: RawRule<ToAbilityTypes<A>, C>;
   public readonly priority!: number;
 
   constructor(
@@ -57,7 +57,7 @@ export class Rule<A extends Abilities, C> {
     this.inverted = !!rule.inverted;
     this.conditions = rule.conditions;
     this.reason = rule.reason;
-    this.additionalData = rule.additionalData;
+    this.origin = rule;
     this.fields = rule.fields ? wrapArray(rule.fields) : undefined;
     this.priority = priority;
     this._options = options;
