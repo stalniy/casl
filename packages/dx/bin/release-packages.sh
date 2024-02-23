dd97c3ef234f685ae7baa276b66909ee320a053c
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2024-02-23 21:14:57 +0200

    chore: splits release-packages script into 2 separate functions

--- packages/dx/bin/release-packages.sh
+++ packages/dx/bin/release-packages.sh
@@ -1,41 +1,47 @@
 #!/usr/bin/env bash
 
-changed_paths=$1;
+release_packages() {
+  changed_paths=$1;
 
-if [ "$changed_paths" = "" ];then
-    echo '
-    Usage:
-      ./release-packages.sh "packages/casl-ability"
-      ./release-packages.sh '
-        packages/casl-ability
-        packages/casl-angular
-      '
-    ';
-    exit 1;
-fi
+  if [ "$changed_paths" = "" ];then
+      echo <<<______HERE__
+      Usage:
+        release-packages "packages/casl-ability"
+        release-packages '
+          packages/casl-ability
+          packages/casl-angular
+        '
+______HERE__;
+      return 1;
+  fi
 
-changed_packages="$(echo "$changed_paths" | grep 'packages/' | grep -v packages/dx)";
+  changed_packages="$(echo "$changed_paths" | grep 'packages/' | grep -v packages/dx)";
 
-if [ "$changed_packages" = "" ]; then
-    echo "No packages to release" >> $GITHUB_STEP_SUMMARY;
-    echo -e "Changed files:\n${changed_paths}" >> $GITHUB_STEP_SUMMARY
-    exit 0;
-fi
+  if [ "$changed_packages" = "" ]; then
+      echo "No packages to release" >> $GITHUB_STEP_SUMMARY;
+      echo -e "Changed files:\n${changed_paths}" >> $GITHUB_STEP_SUMMARY
+      return 0;
+  fi
 
-pnpm_options=''
-for path in $changed_packages; do
-    pnpm_options="${pnpm_options} --filter ./${path}"
-done
+  pnpm_options=''
+  for path in $changed_packages; do
+      pnpm_options="${pnpm_options} --filter ./${path}"
+  done
 
-echo "running: pnpm run -r $pnpm_options release" >> $GITHUB_STEP_SUMMARY
-pnpm run -r $pnpm_options release
+  echo "running: pnpm run -r $pnpm_options release" >> $GITHUB_STEP_SUMMARY
+  pnpm run -r $pnpm_options release
+}
 
-released_packages=();
-for path in $changed_packages; do
-    package_name=$(grep '"name":' $path/package.json | cut -d : -f 2 | cut -d '"' -f 2);
-    package_version=$(grep '"version":' $path/package.json | cut -d : -f 2 | cut -d '"' -f 2);
-    released_packages[${#released_packages[@]}]="${package_name}@${package_version}";
-done
+extract_package_versions() {
+  changed_paths=$1;
 
-echo "🚀 Released in ${released_packages[@]}" >> $GITHUB_STEP_SUMMARY
-echo "${released_packages[@]}"
+  released_packages=();
+  for path in $changed_packages; do
+      package_name=$(grep '"name":' $path/package.json | cut -d : -f 2 | cut -d '"' -f 2);
+      package_version=$(grep '"version":' $path/package.json | cut -d : -f 2 | cut -d '"' -f 2);
+      released_packages[${#released_packages[@]}]="${package_name}@${package_version}";
+  done
+
+  echo "🚀 Released in ${released_packages[@]}" >> $GITHUB_STEP_SUMMARY
+  echo "${released_packages[@]}"
+}
