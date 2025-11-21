7266ce26db68a40eb276a2f3a05b764a9187414b
jaybell <jaycooperbell@gmail.com>
2025-11-21 12:11:41 -0800

    feat: export the Rule and RuleOptions types
    Currently, we deep import `Rule` but with the new `"moduleResolution": "bundler"` this does not work as it is not exported from the `package.json`, this will allow users to correctly import the `Rule` and `RuleOptions` types.

--- packages/casl-ability/src/index.ts
+++ packages/casl-ability/src/index.ts
@@ -3,6 +3,7 @@ export * from './PureAbility';
 export * from './AbilityBuilder';
 export * from './ForbiddenError';
 export * from './RawRule';
+export * from './Rule';
 export * from './hkt';
 export * from './matchers/conditions';
 export * from './matchers/field';
