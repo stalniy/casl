925f1c46535cbbaa606629b961f736c6970bf58b
Serhii Stotskyi <1159966+stalniy@users.noreply.github.com>
2026-04-23 10:05:17 +0200

    fix: ensure that rulesToQuery and rulesToAST generate condition that respect rule priority (#1193)
    BREAKING CHANGE: `rulesToQuery` was replaced with `rulesToCondition`.
    
    Before:
    rulesToQuery(ability, action, subject, convert)
    
    After:
    rulesToCondition(ability.rulesFor(action, subject), convert, {
      and: conditions => ({ $and: conditions }),
      or: conditions => ({ $or: conditions }),
      empty: () => ({})
    })

--- packages/casl-ability/spec/rulesToCondition.spec.ts
+++ packages/casl-ability/spec/rulesToCondition.spec.ts
@@ -1,18 +1,22 @@
-import { createMongoAbility, defineAbility } from '../src'
-import { rulesToQuery } from '../src/extra'
+import { AnyAbility, createMongoAbility, defineAbility, RuleOf } from '../src'
+import { rulesToCondition } from '../src/extra'
 
-function toQuery(ability, action, subject) {
-  const convert = rule => rule.inverted ? { $not: rule.conditions } : rule.conditions
-  return rulesToQuery(ability, action, subject, convert)
+function toQuery<T extends AnyAbility>(ability: T, action: string, subject: string) {
+  const convert = (rule: RuleOf<T>) => rule.inverted ? { $not: rule.conditions } : rule.conditions
+  return rulesToCondition(ability.rulesFor(action, subject), convert, {
+    and: (conditions) => ({ $and: conditions }),
+    or: (conditions) => ({ $or: conditions }),
+    empty: () => ({}),
+  })
 }
 
-describe('rulesToQuery', () => {
+describe(rulesToCondition.name, () => {
   it('returns empty object if there are no rules with conditions', () => {
     const ability = defineAbility(can => can('read', 'Post'))
     const query = toQuery(ability, 'read', 'Post')
 
     expect(query).toBeInstanceOf(Object)
-    expect(Object.keys(query)).toHaveLength(0)
+    expect(Object.keys(query!)).toHaveLength(0)
   })
 
   it('returns empty `$or` part if at least one regular rule does not have conditions', () => {
@@ -23,7 +27,7 @@ describe('rulesToQuery', () => {
     const query = toQuery(ability, 'read', 'Post')
 
     expect(query).toBeInstanceOf(Object)
-    expect(Object.keys(query)).toHaveLength(0)
+    expect(Object.keys(query!)).toHaveLength(0)
   })
 
   it('returns empty `$or` part if rule with conditions defined last', () => {
@@ -34,7 +38,7 @@ describe('rulesToQuery', () => {
     const query = toQuery(ability, 'read', 'Post')
 
     expect(query).toBeInstanceOf(Object)
-    expect(Object.keys(query)).toHaveLength(0)
+    expect(Object.keys(query!)).toHaveLength(0)
   })
 
   it('returns `null` if empty `Ability` instance is passed', () => {
@@ -90,6 +94,20 @@ describe('rulesToQuery', () => {
     })
   })
 
+  it('keeps higher priority regular rules when lower priority inverted rule is unconditional', () => {
+    const ability = defineAbility((can, cannot) => {
+      cannot('read', 'Post')
+      can('read', 'Post', { author: 123 })
+    })
+    const query = toQuery(ability, 'read', 'Post')
+
+    expect(query).toEqual({
+      $or: [
+        { author: 123 }
+      ]
+    })
+  })
+
   it('OR-s conditions for regular rules', () => {
     const ability = defineAbility((can) => {
       can('read', 'Post', { status: 'draft', createdBy: 'someoneelse' })
@@ -132,13 +150,77 @@ describe('rulesToQuery', () => {
 
     expect(query).toEqual({
       $or: [
-        { state: 'draft' },
-        { _id: 'mega' }
-      ],
-      $and: [
-        { $not: { state: 'archived' } },
-        { $not: { private: true } }
+        {
+          $and: [
+            { state: 'draft' },
+            { $not: { state: 'archived' } },
+            { $not: { private: true } }
+          ]
+        },
+        {
+          $and: [
+            { _id: 'mega' },
+            { $not: { state: 'archived' } },
+            { $not: { private: true } }
+          ]
+        }
       ]
+    })
+  })
+
+  it('builds query for a complex mix of priority ordered regular and inverted rules', () => {
+    const ability = defineAbility((can, cannot) => {
+      can('read', 'Post', { ownerId: 1 })
+      cannot('read', 'Post', { archived: true })
+      can('read', 'Post', { shared: true })
+      cannot('read', 'Post', { hidden: true })
+      can('read', 'Post', { public: true })
+      cannot('read', 'Post', { groupped: true })
+      cannot('read', 'Post', { flagged: true })
+      can('read', 'Post', { status: 'pinned' })
+    })
+    const query = toQuery(ability, 'read', 'Post')
+
+    expect(query).toEqual({
+      $or: [
+        { status: 'pinned' },
+        {
+          $and: [
+            { public: true },
+            { $not: { flagged: true } },
+            { $not: { groupped: true } },
+          ]
+        },
+        {
+          $and: [
+            { shared: true },
+            { $not: { flagged: true } },
+            { $not: { groupped: true } },
+            { $not: { hidden: true } }
+          ]
+        },
+        {
+          $and: [
+            { ownerId: 1 },
+            { $not: { flagged: true } },
+            { $not: { groupped: true } },
+            { $not: { hidden: true } },
+            { $not: { archived: true } }
+          ]
+        }
+      ]
+    })
+  })
+
+  it('ignores inverted rules that are overridden by higher priority regular rules', () => {
+    const ability = defineAbility((can, cannot) => {
+      cannot('manage', 'Post', { id: 1 })
+      can('manage', 'Post', { id: 1 })
+    })
+    const query = toQuery(ability, 'manage', 'Post')
+
+    expect(query).toEqual({
+      $or: [{ id: 1 }]
     })
   })
 
@@ -151,10 +233,30 @@ describe('rulesToQuery', () => {
     const query = toQuery(ability, 'read', 'Post')
 
     expect(query).toBeInstanceOf(Object)
-    expect(Object.keys(query)).toHaveLength(0)
+    expect(Object.keys(query!)).toHaveLength(0)
   })
 
-  it('should ignore inverted rules with fields and conditions', () => {
+  it('adds a fallback branch bounded by higher priority inverted rules for lower priority unconditional regular rule', () => {
+    const ability = defineAbility((can, cannot) => {
+      can('read', 'Post')
+      cannot('read', 'Post', { private: true })
+      can('read', 'Post', { author: 123 })
+    })
+    const query = toQuery(ability, 'read', 'Post')
+
+    expect(query).toEqual({
+      $or: [
+        { author: 123 },
+        {
+          $and: [
+            { $not: { private: true } }
+          ]
+        }
+      ]
+    })
+  })
+
+  it('ignores inverted rules with fields and conditions', () => {
     const ability = defineAbility((can, cannot) => {
       can('read', 'Post', { author: 123 })
       cannot('read', 'Post', 'description', { private: true })
@@ -166,7 +268,7 @@ describe('rulesToQuery', () => {
     })
   })
 
-  it('should ignore inverted rules with fields and without conditions', () => {
+  it('ignores inverted rules with fields and without conditions', () => {
     const ability = defineAbility((can, cannot) => {
       can('read', 'Post', { author: 123 })
       cannot('read', 'Post', 'description')
